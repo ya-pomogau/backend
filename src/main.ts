81d38bf469968fcead3c9d9b2e082659122aa2b9
Yury Torgashin <imaginarylight@yandex.ru>
2023-08-02 22:09:08 +0300

    подключить Mongo, создать модель задач

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import configuration from './config/configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(configuration().server.port);
 }
 bootstrap();
