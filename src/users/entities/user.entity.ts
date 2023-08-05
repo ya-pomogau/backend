4470fddc5a1345ec0714b7207c84006b798df9ed
Avok@to34 <vladlen-17@yandex.ru>
2023-08-05 12:06:34 +0300

    feat: добавил модель юзера в ветку разработки

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,87 @@
+import { Length, IsString, IsUrl, IsDate } from 'class-validator';
+import {
+  Entity,
+  ObjectIdColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ObjectId,
+} from 'typeorm';
+
+export enum UserRole {
+  CHIEF = 'Главный администратор',
+  ADMIN = 'Администратор',
+  RECIPIENT = 'Реципиент',
+  VOLUNTEER = 'Волонтер',
+}
+
+@Entity()
+export class User {
+  @ObjectIdColumn()
+  _id: ObjectId;
+
+  @Column()
+  @IsString()
+  @Length(2, 20)
+  fullname: string;
+
+  @Column()
+  @IsString()
+  role: UserRole;
+
+  @Column()
+  @IsUrl()
+  vk: string;
+
+  @Column()
+  @IsUrl()
+  photo: string;
+
+  @Column()
+  @IsString()
+  phone: string;
+
+  @Column()
+  @IsString()
+  address: string;
+
+  @Column()
+  @IsString()
+  coordinates: number[];
+
+  @Column()
+  approved: boolean;
+
+  @Column()
+  checked: boolean;
+
+  @Column()
+  keys: boolean;
+
+  @Column()
+  adminStatus: number;
+
+  @Column()
+  scores: number;
+
+  @Column()
+  completed: number;
+
+  @CreateDateColumn()
+  @IsDate()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  @IsDate()
+  updatedAt: Date;
+
+  //Примерная логика связи сообщений
+
+  // @OneToMany(() => Message, (message) => task.owner)
+  // messages: message[];
+
+  //У тасок один волонтер(пока, м.б. нужна будет возможность нескольких назначать)
+
+  // @OneToMany(() => Tasks, (task) => task.owner)
+  // tasks: task[];
+}
