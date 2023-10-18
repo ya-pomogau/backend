94e4e9e2813cb95601147461896413c9664a0b80
Alexey Yanchev <yanchevalexey@yandex.ru>
2023-10-18 17:20:22 +0300

    Сделал общий тип в common

--- src/datalake/users/dto/create-user.dto.ts
+++ src/datalake/users/dto/create-user.dto.ts
@@ -1,1 +1,23 @@
-export class CreateUserDto {}
+import { PointGeoJSONDocument } from '../../../common/schemas/PointGeoJSON.schema';
+import { UserRole, UserProfile } from '../../../common/types/user.types';
+import { UserStatus, AdminPermission } from '../../../users/types';
+
+export class CreateUserDto {
+  role: UserRole;
+
+  profile: UserProfile;
+
+  vk_id: string;
+
+  location: PointGeoJSONDocument;
+
+  score: number | null;
+
+  status: UserStatus;
+
+  administrative: null | {
+    permissions: AdminPermission[];
+    login: string;
+    password: string;
+  };
+}
