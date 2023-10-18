fe1fc0108851fb87888db74cb5bc0c0a9164d917
Alexey Yanchev <yanchevalexey@yandex.ru>
2023-10-18 22:24:24 +0300

    common dto

--- src/common/types/BlogPostDataDTO.ts
+++ src/common/types/BlogPostDataDTO.ts
@@ -1,3 +1,6 @@
 import { CreateBlogPostDto } from '../../datalake/blog-post/dto/create-blog-post.dto';
+import { BlogPost } from '../../datalake/blog-post/schema/blog-post.schema';
 
-export class BlogPostDataDTO extends CreateBlogPostDto {}
+export class BlogPostDataDTO extends BlogPost {
+  _id: string;
+}
