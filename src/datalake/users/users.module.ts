ebbdbec3bde86a5b709e6157f51742a5d54841b2
Alexey Yanchev <yanchevalexey@yandex.ru>
2023-10-18 17:07:17 +0300

    Написал модуль для User

--- src/datalake/users/users.module.ts
+++ src/datalake/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { UserSchema, User } from './schemas/user.schema';
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService]
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
