a402765afee75f2088ffbf024dafb08aad9c28be
Alexey Yanchev <yanchevalexey@yandex.ru>
2023-10-18 21:24:47 +0300

    Добавил поле _id в общую DTO

--- src/common/types/ContactsDataDTO.ts
+++ src/common/types/ContactsDataDTO.ts
@@ -1,3 +1,6 @@
+import mongoose from 'mongoose';
 import { CreateContactDto } from '../../datalake/contacts/dto/create-contact.dto';
 
-export class ContactsDataDTO extends CreateContactDto {}
+export class ContactsDataDTO extends CreateContactDto {
+  _id: mongoose.Types.ObjectId;
+}
