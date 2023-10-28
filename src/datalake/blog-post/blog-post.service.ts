c8a30974111a3777d874a6503677725269a23468
Alexey Yanchev <yanchevalexey@yandex.ru>
2023-10-28 13:12:02 +0300

    добавил nest исключение

--- src/datalake/blog-post/blog-post.service.ts
+++ src/datalake/blog-post/blog-post.service.ts
@@ -1,16 +1,20 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import exceptions from 'src/common/constants/exceptions';
-import { CreateBlogPostDto } from './dto/create-blog-post.dto';
-import { UpdateBlogPostDto } from './dto/update-blog-post.dto';
-import { BlogPost } from './schema/blog-post.schema';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import exceptions from "src/common/constants/exceptions";
+import { CreateBlogPostDto } from "./dto/create-blog-post.dto";
+import { UpdateBlogPostDto } from "./dto/update-blog-post.dto";
+import { BlogPost } from "./schema/blog-post.schema";
 
 @Injectable()
 export class BlogPostService {
-  constructor(@InjectModel(BlogPost.name) private BlogPostModel: Model<BlogPost>) {}
+  constructor(
+    @InjectModel(BlogPost.name) private BlogPostModel: Model<BlogPost>
+  ) {}
 
-  async create(createBlogPostDto: CreateBlogPostDto): Promise<CreateBlogPostDto> {
+  async create(
+    createBlogPostDto: CreateBlogPostDto
+  ): Promise<CreateBlogPostDto> {
     const createdPost = new this.BlogPostModel(createBlogPostDto);
     const savedPost = await createdPost.save();
     return savedPost.toObject();
@@ -22,21 +26,24 @@ export class BlogPostService {
 
   async findOne(id: string): Promise<BlogPost> {
     return this.BlogPostModel.findById(id)
-      .orFail(new Error(exceptions.blogArticles.notFound))
+      .orFail(new NotFoundException(exceptions.blogArticles.notFound))
       .lean()
       .exec();
   }
 
-  async update(id: string, updateBlogPostDto: UpdateBlogPostDto): Promise<UpdateBlogPostDto> {
+  async update(
+    id: string,
+    updateBlogPostDto: UpdateBlogPostDto
+  ): Promise<UpdateBlogPostDto> {
     return this.BlogPostModel.findByIdAndUpdate(id, updateBlogPostDto)
-      .orFail(new Error(exceptions.blogArticles.notFound))
+      .orFail(new NotFoundException(exceptions.blogArticles.notFound))
       .lean()
       .exec();
   }
 
   async remove(id: string): Promise<BlogPost> {
     return this.BlogPostModel.findByIdAndDelete(id)
-      .orFail(new Error(exceptions.blogArticles.notFound))
+      .orFail(new NotFoundException(exceptions.blogArticles.notFound))
       .lean()
       .exec();
   }
