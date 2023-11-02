afaca6a05d591505405efede248963e2317893fe
Evgeny Karpel <kspshnik@yandex.ru>
2023-11-02 18:29:23 +0300

    [New] Inject repostory

--- src/datalake/task/task.service.ts
+++ src/datalake/task/task.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Task, TaskSchema } from './schemas/task.schema';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  constructor(@InjectModel(Task.name) private readonly taskModel: Model<Task>) {
+    console.dir(typeof Task.prototype);
+    console.dir(typeof TaskSchema);
+  }
+}
