e087c4929c3d4f2c13d935adfd29744962c948a7
Alexey Yanchev <yanchevalexey@yandex.ru>
2023-11-05 16:13:44 +0300

    удалил схемы и дискриминаторы для мастера и визитера

--- src/datalake/users/users.module.ts
+++ src/datalake/users/users.module.ts
@@ -4,9 +4,7 @@ import { HashModule } from '../../common/hash/hash.module';
 import { UsersService } from './users.service';
 import { UserSchema, User } from './schemas/user.schema';
 import { AdminUserSchema } from './schemas/admin.schema';
-import { MasterUserSchema } from './schemas/master.schema';
 import { RecipientUserSchema } from './schemas/recipient.schema';
-import { VisitorUserSchema } from './schemas/visitor.schema';
 import { VolunteerUserSchema } from './schemas/volunteer.schema';
 import { UserRole } from '../../common/types/user.types';
 
@@ -18,9 +16,7 @@ import { UserRole } from '../../common/types/user.types';
         schema: UserSchema,
         discriminators: [
           { name: UserRole.ADMIN, schema: AdminUserSchema },
-          { name: UserRole.MASTER, schema: MasterUserSchema },
           { name: UserRole.RECIPIENT, schema: RecipientUserSchema },
-          { name: UserRole.VISITOR, schema: VisitorUserSchema },
           { name: UserRole.VOLUNTEER, schema: VolunteerUserSchema },
         ],
       },
