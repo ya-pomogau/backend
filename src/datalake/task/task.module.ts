5b79179af7c4dcb2ef14c2a309f81b3a678a7726
Evgeny Karpel <kspshnik@yandex.ru>
2023-11-16 21:38:35 +0300

    [Hotfix] Remove unused import of absent TaskSchema

--- src/datalake/task/task.module.ts
+++ src/datalake/task/task.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TaskService } from './task.repository';
-import { Task, TaskSchema } from './schemas/task.schema';
+import { Task } from './schemas/task.schema';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }])],
