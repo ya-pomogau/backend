b36c31fa69ee8409044e7a6c9d6713b2f2ac7ed4
Evgeny Karpel <kspshnik@yandex.ru>
2023-11-16 21:37:06 +0300

    [Hotfix] Rename to task.repository.ts

--- src/datalake/task/task.module.ts
+++ src/datalake/task/task.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { TaskService } from './task.service';
+import { TaskService } from './task.repository';
 import { Task, TaskSchema } from './schemas/task.schema';
 
 @Module({
