2bdfa82df26b58b3f9c063744093bb37af198941
Evgeny Karpel <kspshnik@yandex.ru>
2023-11-17 16:32:12 +0300

    [Refactor] Category Entity via extending BaseRepository

---
+++ src/datalake/category/schemas/category.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { UserStatus } from '../../../users/types';
+import { CategoryInterface } from '../../../common/types/category.types';
+
+@Schema({ timestamps: true })
+export class Category extends Document implements CategoryInterface {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true, type: Number })
+  points: number;
+
+  @Prop({ required: true, enum: Object.values(UserStatus), type: Number })
+  accessLevel: number;
+}
+
+export const CategorySchema = SchemaFactory.createForClass(Category);
