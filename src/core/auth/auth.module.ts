645bc5a3f644b954ad6fc715bb77222d89c89319
Evgeny Karpel <kspshnik@yandex.ru>
2023-12-21 16:46:24 +0300

    [Fix] Corrected imports/providers
    Signed-off-by: Evgeny Karpel <kspshnik@yandex.ru>

--- src/core/auth/auth.module.ts
+++ src/core/auth/auth.module.ts
@@ -2,14 +2,18 @@ import { Module } from '@nestjs/common';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 
 import { UsersModule } from '../users/users.module';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   imports: [
     UsersModule,
     HttpModule,
+    PassportModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
@@ -19,6 +23,6 @@ import { UsersModule } from '../users/users.module';
       inject: [ConfigService],
     }),
   ],
-  providers: [AuthService, JwtService],
+  providers: [AuthService, JwtService, JwtStrategy, LocalStrategy],
 })
 export class AuthModule {}
