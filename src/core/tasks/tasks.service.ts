35c32140fe255f7ce4a44ac9385e5dc9f0d56926
Evgeny Karpel <kspshnik@yandex.ru>
2024-01-31 21:36:11 +0300

    [Feature] Created status/keys manipulation handles (#60)
    Admin-api handles to grant/revoke keys (/admin/:id/keys), activate/deactivate admins (/admin/:id/activate) & confirm or block users (/admin/:id/confirm), and respective `UsersService` methods.
    Allows action (grant keys, confirm or activate) by PUT request, contrary (revoke keys, block or deactivate) by DELETE request.
    
    Commits included:
    
    * [Refactor] Added BLOCKED status w/value of -1 to UserStatus
    
    Signed-off-by: Evgeny Karpel <kspshnik@yandex.ru>
    
    * [Refactor] Corrected user (`name` instead of first, middle & last) & admin (added `isActive` boolean props) schemas
    
    Signed-off-by: Evgeny Karpel <kspshnik@yandex.ru>
    
    * [Refactor] Corrected user (`name` instead of first, middle & last) & admin (added `isActive` boolean props) schemas. Fixed Tasks service changes as well.
    
    Signed-off-by: Evgeny Karpel <kspshnik@yandex.ru>
    
    * [New] Created `promoteUser()`
    
    Signed-off-by: Evgeny Karpel <kspshnik@yandex.ru>
    
    * [New] Created `promoteUser()`
    
    Signed-off-by: Evgeny Karpel <kspshnik@yandex.ru>
    
    * [New] Created `activate()` & `block()`
    
    Signed-off-by: Evgeny Karpel <kspshnik@yandex.ru>
    
    * [Feature] Created handles to confirm, block, promote users, hand keys to volunteers & activate/deactivate admins
    
    Signed-off-by: Evgeny Karpel <kspshnik@yandex.ru>
    
    * [Fix] Misprint fixed
    
    Signed-off-by: Evgeny Karpel <kspshnik@yandex.ru>
    
    * [Fix] Clean unused commented code
    
    Signed-off-by: Evgeny Karpel <kspshnik@yandex.ru>
    
    ---------
    
    Signed-off-by: Evgeny Karpel <kspshnik@yandex.ru>

--- src/core/tasks/tasks.service.ts
+++ src/core/tasks/tasks.service.ts
@@ -1,4 +1,115 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { FilterQuery } from 'mongoose';
+import { TasksRepository } from '../../datalake/task/task.repository';
+import { UsersRepository } from '../../datalake/users/users.repository';
+import { CreateTaskDto, GetVirginTasksDto } from '../../common/dto/tasks.dto';
+import { Recipient } from '../../datalake/users/schemas/recipient.schema';
+import { Admin } from '../../datalake/users/schemas/admin.schema';
+import { CategoryRepository } from '../../datalake/category/category.repository';
+import { Task } from '../../datalake/task/schemas/task.schema';
+import { ResolveStatus } from '../../common/types/task.types';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(
+    private readonly tasksRepo: TasksRepository,
+    private readonly usersRepo: UsersRepository,
+    private readonly categoryRepo: CategoryRepository
+  ) {}
+
+  public async create(dto: CreateTaskDto) {
+    const { recipientId, categoryId, ...data } = dto;
+    const recipient = await this.usersRepo.findById(recipientId);
+    const category = await this.categoryRepo.findById(categoryId);
+    if (!(recipient instanceof Recipient || recipient instanceof Admin)) {
+      throw new ForbiddenException('Только реципиент или администратор могут создавать заявки', {
+        cause: `Попытка создать заявку пользователем с _id ${recipientId} и ролью волонтёра`,
+      });
+    }
+    const { name, phone, avatar, address, _id } = recipient;
+    const task = {
+      ...data,
+      recipient: { name, phone, avatar, address, _id },
+      volunteer: null,
+      category,
+      isPendingChanges: false,
+    };
+    return this.tasksRepo.create(task);
+  }
+
+  public async getVirginTasks(dto: GetVirginTasksDto) {
+    const {
+      location: { coordinates: center },
+      distance,
+      start,
+      end,
+      categoryId,
+    } = dto;
+    const query: FilterQuery<Task> = {
+      volunteerReport: ResolveStatus.VIRGIN,
+      recipientReport: ResolveStatus.VIRGIN,
+      location: {
+        $near: {
+          $geometry: center,
+          $maxDistance: distance,
+        },
+      },
+    };
+    if (categoryId) {
+      query.category = await this.categoryRepo.findById(categoryId);
+    }
+    if (!!start && !!end) {
+      query.date = {
+        $gte: start,
+        $lte: end,
+      };
+    } else if (!!start && !end) {
+      query.date = {
+        $gte: start,
+      };
+    } else if (!start && !!end) {
+      query.date = {
+        $lte: end,
+      };
+    }
+    return this.tasksRepo.find(query);
+  }
+
+  public async getAcceptedTasks(dto: GetVirginTasksDto) {
+    const {
+      location: { coordinates: center },
+      distance,
+      start,
+      end,
+      categoryId,
+    } = dto;
+    const query: FilterQuery<Task> = {
+      volunteerReport: ResolveStatus.VIRGIN,
+      recipientReport: ResolveStatus.VIRGIN,
+      location: {
+        $near: {
+          $geometry: center,
+          $maxDistance: distance,
+        },
+      },
+    };
+    if (categoryId) {
+      query.category = await this.categoryRepo.findById(categoryId);
+    }
+    if (!!start && !!end) {
+      query.date = {
+        $gte: start,
+        $lte: end,
+      };
+    } else if (!!start && !end) {
+      query.date = {
+        $gte: start,
+      };
+    } else if (!start && !!end) {
+      query.date = {
+        $lte: end,
+      };
+    }
+    return this.tasksRepo.find(query);
+  }
+}
