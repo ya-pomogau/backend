32c5f0502f00c1e072d88e147327dac99136a70d
Evgeny Karpel <kspshnik@yandex.ru>
2024-02-01 15:00:25 +0300

    [New] Core module methods to create, find & accept tasks.
    Signed-off-by: Evgeny Karpel <kspshnik@yandex.ru>

--- src/core/tasks/tasks.service.ts
+++ src/core/tasks/tasks.service.ts
@@ -1,13 +1,16 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import { ConflictException, ForbiddenException, Injectable } from '@nestjs/common';
 import { FilterQuery } from 'mongoose';
 import { TasksRepository } from '../../datalake/task/task.repository';
 import { UsersRepository } from '../../datalake/users/users.repository';
-import { CreateTaskDto, GetVirginTasksDto } from '../../common/dto/tasks.dto';
+import { CreateTaskDto, GetTasksDto } from '../../common/dto/tasks.dto';
 import { Recipient } from '../../datalake/users/schemas/recipient.schema';
 import { Admin } from '../../datalake/users/schemas/admin.schema';
 import { CategoryRepository } from '../../datalake/category/category.repository';
 import { Task } from '../../datalake/task/schemas/task.schema';
-import { ResolveStatus } from '../../common/types/task.types';
+import { ResolveStatus, TaskStatus } from '../../common/types/task.types';
+import { UserRole } from '../../common/types/user.types';
+import { Volunteer } from '../../datalake/users/schemas/volunteer.schema';
+import { User } from '../../datalake/users/schemas/user.schema';
 
 @Injectable()
 export class TasksService {
@@ -21,7 +24,8 @@ export class TasksService {
     const { recipientId, categoryId, ...data } = dto;
     const recipient = await this.usersRepo.findById(recipientId);
     const category = await this.categoryRepo.findById(categoryId);
-    if (!(recipient instanceof Recipient || recipient instanceof Admin)) {
+    /* !(recipient instanceof Recipient || recipient instanceof Admin) */
+    if (![`${UserRole.ADMIN}`, `${UserRole.RECIPIENT}`].includes(recipient.role)) {
       throw new ForbiddenException('Только реципиент или администратор могут создавать заявки', {
         cause: `Попытка создать заявку пользователем с _id ${recipientId} и ролью волонтёра`,
       });
@@ -31,13 +35,14 @@ export class TasksService {
       ...data,
       recipient: { name, phone, avatar, address, _id },
       volunteer: null,
+      status: TaskStatus.CREATED,
       category,
       isPendingChanges: false,
     };
     return this.tasksRepo.create(task);
   }
 
-  public async getVirginTasks(dto: GetVirginTasksDto) {
+  public async getNotAcceptedTasks(dto: GetTasksDto) {
     const {
       location: { coordinates: center },
       distance,
@@ -46,8 +51,7 @@ export class TasksService {
       categoryId,
     } = dto;
     const query: FilterQuery<Task> = {
-      volunteerReport: ResolveStatus.VIRGIN,
-      recipientReport: ResolveStatus.VIRGIN,
+      volunteer: null,
       location: {
         $near: {
           $geometry: center,
@@ -56,7 +60,7 @@ export class TasksService {
       },
     };
     if (categoryId) {
-      query.category = await this.categoryRepo.findById(categoryId);
+      query.category._id = categoryId;
     }
     if (!!start && !!end) {
       query.date = {
@@ -75,7 +79,7 @@ export class TasksService {
     return this.tasksRepo.find(query);
   }
 
-  public async getAcceptedTasks(dto: GetVirginTasksDto) {
+  public async getAcceptedTasks(dto: GetTasksDto) {
     const {
       location: { coordinates: center },
       distance,
@@ -84,8 +88,7 @@ export class TasksService {
       categoryId,
     } = dto;
     const query: FilterQuery<Task> = {
-      volunteerReport: ResolveStatus.VIRGIN,
-      recipientReport: ResolveStatus.VIRGIN,
+      status: TaskStatus.ACCEPTED,
       location: {
         $near: {
           $geometry: center,
@@ -94,7 +97,7 @@ export class TasksService {
       },
     };
     if (categoryId) {
-      query.category = await this.categoryRepo.findById(categoryId);
+      query.category._id = categoryId;
     }
     if (!!start && !!end) {
       query.date = {
@@ -111,5 +114,29 @@ export class TasksService {
       };
     }
     return this.tasksRepo.find(query);
+  }
+
+  async acceptTask(taskId: string, volunteerId: string) {
+    const volunteer = (await this.usersRepo.findById(volunteerId)) as User & Volunteer;
+    if (![`${UserRole.ADMIN}`, `${UserRole.VOLUNTEER}`].includes(volunteer.role)) {
+      throw new ForbiddenException('Только волонтёр или администратор могут создавать заявки', {
+        cause: `Попытка создать заявку пользователем с _id '${volunteerId}' и ролью '${volunteer.role}'`,
+      });
+    }
+    const task = await this.tasksRepo.findById(taskId);
+    if (task.volunteer) {
+      throw new ConflictException('Эта заявка уже взята другим волонтёром', {
+        cause: `Попытка повторно взять заявку с _id '${taskId}' пользователем с _id '${volunteerId}' и ролью '${volunteer.role}'`,
+      });
+    }
+    if (volunteer.status < task.category.accessLevel) {
+      throw new ForbiddenException('Вам нельзя брать задачи из этой категории!');
+    }
+    const { name, phone, avatar, address, _id } = volunteer;
+    return this.tasksRepo.findByIdAndUpdate(
+      taskId,
+      { status: TaskStatus.ACCEPTED, volunteer: { name, phone, avatar, address, _id } },
+      { new: true }
+    );
   }
 }
