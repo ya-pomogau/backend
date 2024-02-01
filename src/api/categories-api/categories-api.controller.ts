0a50761d3c2321ef63c4c3c026681824b4f61c77
ulbee <p.kotta@yandex.ru>
2024-02-01 16:07:06 +0300

    Закомментила моки для контроллера

--- src/api/categories-api/categories-api.controller.ts
+++ src/api/categories-api/categories-api.controller.ts
@@ -1,65 +1,59 @@
-import { Body, Controller, Post, Get, UseGuards, Param, Patch, Delete, Req, Query } from '@nestjs/common';
-import { CategoriesService } from 'src/core/categories/categories.service';
-import { NewCategoryDto } from './dto/new-category.dto';
-import { UserRole, AdminPermission } from '../../common/types/user.types';
-import { AccessControlGuard } from '../../common/guards/access-control.guard';
-// import { Root } from '../../common/decorators/root.decorator';
-import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
-// import { Role } from '../../common/decorators/roles.decorator';
-import { AccessControlList } from '../../common/decorators/access-control-list.decorator';
-import { Public } from 'src/common/decorators/public.decorator';
-import { UpdateCategoryDto } from './dto/update-category.dto';
+// import { Body, Controller, Post, Get, UseGuards, Param, Patch, Delete, Req, Query } from '@nestjs/common';
+// import { CategoriesService } from 'src/core/categories/categories.service';
+// import { NewCategoryDto } from './dto/new-category.dto';
+// import { UserRole, AdminPermission } from '../../common/types/user.types';
+// import { AccessControlGuard } from '../../common/guards/access-control.guard';
+// // import { Root } from '../../common/decorators/root.decorator';
+// import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
+// // import { Role } from '../../common/decorators/roles.decorator';
+// import { AccessControlList } from '../../common/decorators/access-control-list.decorator';
+// import { Public } from 'src/common/decorators/public.decorator';
+// import { UpdateCategoryDto } from './dto/update-category.dto';
 
-// @UseGuards(JwtAuthGuard)
-// @UseGuards(AccessControlGuard)
+// // @UseGuards(JwtAuthGuard)
+// // @UseGuards(AccessControlGuard)
 
-@Controller('categories')
-export class CategoriesApiController {
-  constructor(private readonly categoriesService: CategoriesService) {}
+// @Controller('categories')
+// export class CategoriesApiController {
+//   constructor(private readonly categoriesService: CategoriesService) {}
 
-  @Public()
-  @Get()
-  // @AccessControlList({ role: UserRole.ADMIN, permissions: AdminPermission.CATEGORIES})
-  async getAll() {
-    return this.categoriesService.getCategories();
-  }
+//   @Public()
+//   @Get()
+//   // @AccessControlList({ role: UserRole.ADMIN, permissions: AdminPermission.CATEGORIES})
+//   async getAll() {
+//     return this.categoriesService.getCategories();
+//   }
 
-  @Public()
-  @Patch()
-  async updateCategories(@Body() data: Record<string, number>, @Req() req) {
-    console.log('TTTTT')
-    return this.categoriesService.updateCategories(data, req.user);
-  }
+//   @Public()
+//   @Patch()
+//   async updateCategories(@Body() data: Record<string, number>, @Req() req) {
+//     console.log('TTTTT')
+//     return this.categoriesService.updateCategories(data, req.user);
+//   }
 
-  @Public()
-  @Get('/:id')
-  // @AccessControlList({ role: UserRole.ADMIN, permissions: AdminPermission.CATEGORIES})
-  async getCategory(@Param('id') id: string) {
-    return this.categoriesService.getCategoryById(id);
-  }
+//   @Public()
+//   @Get('/:id')
+//   // @AccessControlList({ role: UserRole.ADMIN, permissions: AdminPermission.CATEGORIES})
+//   async getCategory(@Param('id') id: string) {
+//     return this.categoriesService.getCategoryById(id);
+//   }
 
-  @Public()
-  @Post()
-  // @AccessControlList({ role: UserRole.ADMIN, permissions: AdminPermission.CATEGORIES})
-  async create(@Body() dto: NewCategoryDto, @Req() req) {
-    return this.categoriesService.createCategory(dto, req.user);
-  }
+//   @Public()
+//   @Post()
+//   // @AccessControlList({ role: UserRole.ADMIN, permissions: AdminPermission.CATEGORIES})
+//   async create(@Body() dto: NewCategoryDto, @Req() req) {
+//     return this.categoriesService.createCategory(dto, req.user);
+//   }
 
-  @Public()
-  @Patch('/:id')
-  async update(@Param('id') id: string, @Body() updateData: UpdateCategoryDto, @Req() req) {
-    return this.categoriesService.updateCategoryById(id, updateData, req.user);
-  }
+//   @Public()
+//   @Patch('/:id')
+//   async update(@Param('id') id: string, @Body() updateData: UpdateCategoryDto, @Req() req) {
+//     return this.categoriesService.updateCategoryById(id, updateData, req.user);
+//   }
 
-  @Public()
-  @Delete('/:id')
-  async delete(@Param('id') id: string, @Req() req) {
-    return this.categoriesService.removeCategory(id, req.user);
-  }
-
-
-
-
-
-
-}
+//   @Public()
+//   @Delete('/:id')
+//   async delete(@Param('id') id: string, @Req() req) {
+//     return this.categoriesService.removeCategory(id, req.user);
+//   }
+// }
