8dd841bdb89fae1b8785ac6278e1945402f0100b
Evgeny Karpel <kspshnik@yandex.ru>
2024-02-12 11:30:08 +0300

    [New] Created `TasksService` methods to get own tasks of any status
    Signed-off-by: Evgeny Karpel <kspshnik@yandex.ru>

--- src/core/tasks/tasks.service.ts
+++ src/core/tasks/tasks.service.ts
@@ -5,7 +5,7 @@ import { UsersRepository } from '../../datalake/users/users.repository';
 import { CreateTaskDto, GetTasksDto } from '../../common/dto/tasks.dto';
 import { CategoryRepository } from '../../datalake/category/category.repository';
 import { Task } from '../../datalake/task/schemas/task.schema';
-import { TaskStatus } from '../../common/types/task.types';
+import { ResolveStatus, TaskReport, TaskStatus } from '../../common/types/task.types';
 import { UserRole } from '../../common/types/user.types';
 import { Volunteer } from '../../datalake/users/schemas/volunteer.schema';
 import { User } from '../../datalake/users/schemas/user.schema';
@@ -42,6 +42,7 @@ export class TasksService {
   public async getNotAcceptedTasks(dto: Partial<GetTasksDto>) {
     const { location: center, distance, start, end, categoryId } = dto;
     const query: FilterQuery<Task> = {
+      status: TaskStatus.CREATED,
       volunteer: null,
       location: {
         $near: {
@@ -101,6 +102,41 @@ export class TasksService {
     return this.tasksRepo.find(query);
   }
 
+  async getOwnTasks(userId: string, userRole: UserRole, status: TaskStatus, dto?: GetTasksDto) {
+    const { location: center, distance, start, end, categoryId } = dto;
+    const roleIndex = userRole.toLowerCase();
+    const query: FilterQuery<Task> = {
+      status: TaskStatus.COMPLETED,
+      [roleIndex]: { _id: userId },
+    };
+    if (!!center && center.length === 2 && !!distance) {
+      query.location = {
+        $near: {
+          $geometry: center,
+          $maxDistance: distance,
+        },
+      };
+    }
+    if (categoryId) {
+      query.category._id = categoryId;
+    }
+    if (!!start && !!end) {
+      query.date = {
+        $gte: start,
+        $lte: end,
+      };
+    } else if (!!start && !end) {
+      query.date = {
+        $gte: start,
+      };
+    } else if (!start && !!end) {
+      query.date = {
+        $lte: end,
+      };
+    }
+    return this.tasksRepo.find(query);
+  }
+
   async acceptTask(taskId: string, volunteerId: string) {
     const volunteer = (await this.usersRepo.findById(volunteerId)) as User & Volunteer;
     if (![`${UserRole.ADMIN}`, `${UserRole.VOLUNTEER}`].includes(volunteer.role)) {
@@ -123,5 +159,40 @@ export class TasksService {
       { status: TaskStatus.ACCEPTED, volunteer: { name, phone, avatar, address, _id } },
       { new: true }
     );
+  }
+
+  async reportTask(taskId: string, userId: string, userRole: UserRole, result: TaskReport) {
+    const myIndex = userRole === UserRole.VOLUNTEER ? 'volunteerReport' : 'recipientReport';
+    const counterpartyIndex =
+      userRole === UserRole.RECIPIENT ? 'volunteerReport' : 'recipientReport';
+    const task = await this.tasksRepo.findById(taskId);
+    if (task.status !== TaskStatus.ACCEPTED) {
+      throw new ForbiddenException('Нельзя отчитаться по не открытой задаче!', {
+        cause: `Попытка отчёта по задаче с _id '${task._id}' со статусом ${task.status} `,
+      });
+    }
+    if (task[counterpartyIndex]) {
+      this.tasksRepo.findByIdAndUpdate(
+        taskId,
+        {
+          ...task,
+          [myIndex]: result,
+          status: result === task[counterpartyIndex] ? TaskStatus.COMPLETED : TaskStatus.CONFLICTED,
+          adminResolve: result === task[counterpartyIndex] ? null : ResolveStatus.VIRGIN,
+          isPendingChanges: false,
+        },
+        { new: true }
+      );
+    } else {
+      this.tasksRepo.findByIdAndUpdate(
+        taskId,
+        {
+          ...task,
+          [myIndex]: result,
+          isPendingChanges: true,
+        },
+        { new: true }
+      );
+    }
   }
 }
