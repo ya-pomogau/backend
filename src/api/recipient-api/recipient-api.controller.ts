37f841870987431b841d9bce59a1592e9af1c450
Evgeny Karpel <kspshnik@yandex.ru>
2024-02-13 16:03:47 +0300

    [New] Created task handlers for both volunteer and recipient
    Signed-off-by: Evgeny Karpel <kspshnik@yandex.ru>

--- src/api/recipient-api/recipient-api.controller.ts
+++ src/api/recipient-api/recipient-api.controller.ts
@@ -1,10 +1,23 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { TasksService } from '../../core/tasks/tasks.service';
 import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
 import { AccessControlGuard } from '../../common/guards/access-control.guard';
 import { ApiCreateTaskDto } from './dto/create-task.dto';
 import { AccessControlList } from '../../common/decorators/access-control-list.decorator';
-import { UserRole, UserStatus } from '../../common/types/user.types';
+import { AnyUserInterface, UserRole, UserStatus } from '../../common/types/user.types';
+import { GetTasksSearchDto } from './dto/get-tasks-query.dto';
+import { TaskReport, TaskStatus } from '../../common/types/task.types';
 
 @UseGuards(JwtAuthGuard)
 @UseGuards(AccessControlGuard)
@@ -14,7 +27,53 @@ export class RecipientApiController {
 
   @Post('/tasks')
   @AccessControlList({ role: UserRole.RECIPIENT, level: UserStatus.CONFIRMED })
-  async create(@Body() dto: ApiCreateTaskDto, @Req() { user: { _id: recipientId } }) {
+  public async create(@Body() dto: ApiCreateTaskDto, @Req() { user: { _id: recipientId } }) {
     return this.tasksService.create({ ...dto, recipientId });
+  }
+
+  @Get('/tasks/virgin')
+  @AccessControlList({ role: UserRole.RECIPIENT, level: UserStatus.CONFIRMED })
+  public async getVirginTasks(@Query() query: GetTasksSearchDto) {
+    return this.tasksService.getTasksByStatus(TaskStatus.CREATED, query);
+  }
+
+  @Get('/tasks/:id')
+  @AccessControlList({ role: UserRole.RECIPIENT, level: UserStatus.CONFIRMED })
+  public async getTaskById(@Param(':id') id: string) {
+    return this.tasksService.getTask(id);
+  }
+
+  @Patch('/tasks/:id')
+  @AccessControlList({ role: UserRole.RECIPIENT, level: UserStatus.CONFIRMED })
+  public async updateTask(
+    @Param(':id') id: string,
+    @Req() user,
+    @Body() dto: Partial<ApiCreateTaskDto>
+  ) {
+    return this.tasksService.updateTask(id, user as AnyUserInterface, dto);
+  }
+
+  @Put('/tasks/:id/fulfill')
+  @AccessControlList({ role: UserRole.RECIPIENT, level: UserStatus.CONFIRMED })
+  public async fulfillTask(@Param(':id') id: string, @Req() { user: { _id: userId } }) {
+    return this.tasksService.reportTask(id, userId, UserRole.RECIPIENT, TaskReport.FULFILLED);
+  }
+
+  @Put('/tasks/:id/reject')
+  @AccessControlList({ role: UserRole.RECIPIENT, level: UserStatus.CONFIRMED })
+  public async rejectTask(@Param(':id') id: string, @Req() { user: { _id: userId } }) {
+    return this.tasksService.reportTask(id, userId, UserRole.RECIPIENT, TaskReport.REJECTED);
+  }
+
+  @Get('/tasks/accepted')
+  @AccessControlList({ role: UserRole.RECIPIENT, level: UserStatus.CONFIRMED })
+  public async getAcceptedTasks(@Query() query: GetTasksSearchDto) {
+    return this.tasksService.getTasksByStatus(TaskStatus.ACCEPTED, query);
+  }
+
+  @Get('/tasks/completed')
+  @AccessControlList({ role: UserRole.RECIPIENT, level: UserStatus.CONFIRMED })
+  public async getCompletedTasks(@Query() query: GetTasksSearchDto) {
+    return this.tasksService.getTasksByStatus(TaskStatus.COMPLETED, query);
   }
 }
