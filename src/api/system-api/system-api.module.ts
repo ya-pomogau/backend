7f84300490714f010ac8beb70d541f19b290d70e
ulbee <p.kotta@yandex.ru>
2024-03-12 10:44:49 +0300

    Добавлена ручка получения контактов

--- src/api/system-api/system-api.module.ts
+++ src/api/system-api/system-api.module.ts
@@ -4,9 +4,10 @@ import { SystemApiController } from './system-api.controller';
 import { CategoriesModule } from '../../core/categories/categories.module';
 import { TasksModule } from '../../core/tasks/tasks.module';
 import { UsersModule } from '../../core/users/users.module';
+import { ContactsModule } from '../../core/contacts/contacts.module';
 
 @Module({
-  imports: [BlogModule, CategoriesModule, TasksModule, UsersModule],
+  imports: [BlogModule, CategoriesModule, TasksModule, UsersModule, ContactsModule],
   controllers: [SystemApiController],
   providers: [],
 })
