7e5b6b33e7e130b0a7af15e7dc35ef78247051f8
Alexey Krutyakov <coder.arctik@gmail.com>
2024-06-16 22:36:47 +0300

    feat: add types for chats

--- src/common/types/chats.types.ts
+++ src/common/types/chats.types.ts
@@ -1,16 +1,6 @@
 import { type ObjectId } from 'mongoose';
-import { AnyUserInterface } from './user.types';
-
-// export interface ChatInterface {
-//   _id: ObjectId;
-//   users: [ObjectId, ObjectId | null];
-//   messages: MessageInterface;
-//   isOpen: boolean;
-//   ownerId: ObjectId;
-//   taskId: ObjectId;
-//   createdAt: Date;
-//   updatedAt: Date;
-// }
+import { AnyUserInterface, RecipientInterface, VolunteerInterface } from './user.types';
+import { chats } from '../constants/chats';
 
 export interface MessageInterface {
   _id: ObjectId;
@@ -21,3 +11,43 @@ export interface MessageInterface {
   author: AnyUserInterface;
   chatId: ObjectId;
 }
+
+export type ChatType = keyof typeof chats;
+
+export interface ChatModelInterface {
+  _id: ObjectId;
+  type: ChatType;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface TaskChatModelInterface {
+  volunteer: VolunteerInterface;
+  recipient: RecipientInterface;
+}
+
+export interface SystemChatModelInterface {
+  user: VolunteerInterface | RecipientInterface;
+}
+
+export interface ConflictWithVolunteerChatModelInterface {
+  taskId: ObjectId;
+  volunteer: VolunteerInterface;
+  recipient: RecipientInterface;
+}
+
+export interface ConflictWithRecipientChatModelInterface {
+  taskId: ObjectId;
+  recipient: RecipientInterface;
+  volunteerChat: ObjectId;
+}
+
+export interface TaskChatInterface extends ChatModelInterface, TaskChatModelInterface {}
+
+export interface ConflictWithVolunteerChatInterface
+  extends ChatModelInterface,
+    ConflictWithVolunteerChatModelInterface {}
+
+export interface ConflictWithRecipientChatInterface
+  extends ChatModelInterface,
+    ConflictWithRecipientChatModelInterface {}
