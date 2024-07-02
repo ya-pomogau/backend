c923512b570812435fc01ffe6548ca384b3039cd
ivanna-balanyuk <ivannabalanyuk@yandex.ru>
2024-07-02 12:22:22 +0300

    fix: use correct validations decorators at ApiBulkUpdateCategoriesDto

--- src/api/admin-api/dto/bulk-update-categories.dto.ts
+++ src/api/admin-api/dto/bulk-update-categories.dto.ts
@@ -1,10 +1,23 @@
+/* eslint-disable max-classes-per-file */
+
 import { ApiProperty } from '@nestjs/swagger';
-import { IsArray, IsNotEmpty } from 'class-validator';
-import { ApiBulkUpdateCategoryDto } from './bulk-update-category.dto';
+import { IsArray, IsNotEmpty, IsString, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+import { ApiCreateCategoryDto } from './new-category.dto';
+import { BulkUpdateCategoryInterface } from '../../../common/types/category.types';
+
+class BulkUpdateCategory extends ApiCreateCategoryDto implements BulkUpdateCategoryInterface {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  id: string;
+}
 
 export class ApiBulkUpdateCategoriesDto {
   @ApiProperty()
   @IsNotEmpty()
   @IsArray()
-  data: Array<ApiBulkUpdateCategoryDto>;
+  @ValidateNested({ each: true })
+  @Type(() => BulkUpdateCategory)
+  data: BulkUpdateCategory[];
 }
