ec20f17188897afa16479fe1b1dbec57977b3953
Ivanna Balanyuk <124444858+IvannaBalanyuk@users.noreply.github.com>
2024-07-13 16:58:29 +0300

    [Refactor] Added points and task count increase on fulfilling task for volunteer
    * refactor: add tasksComplited field to VolunteerUserSchema
    
    * refactor: add closeTaskAsCompleted private method
    
    * refactor: add closeTaskAsRejected private method
    
    * fix: add discriminatorKey at findOneAndUpdate() method filter
    
    * fix: delete overwriteDiscriminatorKey option from findOneAndUpdate() at closeTaskAsFulfilled({...})
    
    * hotfixes: add interfaces for props at task.service, make private method for volunteer score updating
    
    * hotfixes: move volunteer updating method to UsersService, add types for results

--- src/core/tasks/tasks.module.ts
+++ src/core/tasks/tasks.module.ts
@@ -3,9 +3,10 @@ import { TasksService } from './tasks.service';
 import { TasksRepositoryModule } from '../../datalake/task/tasks-repository.module';
 import { UsersRepositoryModule } from '../../datalake/users/users-repository.module';
 import { CategoryRepositoryModule } from '../../datalake/category/category-repository.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TasksRepositoryModule, UsersRepositoryModule, CategoryRepositoryModule],
+  imports: [TasksRepositoryModule, UsersRepositoryModule, CategoryRepositoryModule, UsersModule],
   providers: [TasksService],
   exports: [TasksService],
 })
