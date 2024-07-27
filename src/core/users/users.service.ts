d785dd1e3afec1a4ff43e50880d44ed1b2532bee
Evgeny Karpel <kspshnik@yandex.ru>
2024-07-27 11:08:52 +0300

    [Fix] Corrected `UsersService.deactivate()` to work with discriminated collection

--- src/core/users/users.service.ts
+++ src/core/users/users.service.ts
@@ -252,7 +252,11 @@ export class UsersService {
     }
     if (user.role === UserRole.ADMIN) {
       UsersService.requireLogin(_id);
-      return this.usersRepo.findOneAndUpdate({ _id, role: UserRole.ADMIN }, { isActive: true }, {});
+      return this.usersRepo.findOneAndUpdate(
+        { _id, role: UserRole.ADMIN },
+        { isActive: true },
+        { new: true }
+      );
     }
     throw new BadRequestException('Можно активировать только администратора');
   }
@@ -282,7 +286,11 @@ export class UsersService {
     }
     if (user.role === UserRole.ADMIN) {
       UsersService.requireLogin(_id);
-      return this.usersRepo.findByIdAndUpdate(_id, { isActivated: false }, {});
+      return this.usersRepo.findOneAndUpdate(
+        { _id, role: UserRole.ADMIN },
+        { isActive: false },
+        { new: true }
+      );
     }
     if (user.role === UserRole.VOLUNTEER || user.role === UserRole.RECIPIENT) {
       throw new BadRequestException('Нужен _id администратора!');
