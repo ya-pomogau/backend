739c0e62e54c1bae07c5cad6367148213f2a372e
zoomkolol <56260430+zoomkolol@users.noreply.github.com>
2024-09-11 20:36:10 +0300

    Create mock chats to return on creation

--- src/core/chat/chats.service.ts
+++ src/core/chat/chats.service.ts
@@ -1,77 +1,100 @@
 import { Injectable } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { ObjectId } from 'mongodb';
+import mongoose from 'mongoose';
 
 import { ConflictChatsTupleMetaInterface,
   MessageInterface,
+  RecipientConflictChatMetaInterface,
   SystemChatMetaInterface,
-  TaskChatMetaInterface
+  TaskChatMetaInterface,
+  VolunteerConflictChatMetaInterface
 } from 'src/common/types/chats.types';
-import { UserRole, UserStatus } from 'src/common/types/user.types';
+import { AdminInterface, UserRole, UserStatus } from 'src/common/types/user.types';
 import { wsChatPageQueryPayload, wsMessageData } from 'src/common/types/websockets.types';
 
 
 //#region Mock data
-const mockObjectId = new ObjectId().toHexString();
+
+const mockVolunteer = {
+  _id: 'volunteerId',
+  role: UserRole.VOLUNTEER,
+  name: 'Volunteer',
+  phone: 'phone',
+  avatar: 'avatar',
+  address: 'address',
+  vkId: 'vkId',
+  score: 0,
+  status: UserStatus.CONFIRMED,
+  location: undefined,
+  keys: false,
+  tasksCompleted: 0,
+  createdAt: new Date().toISOString(),
+  updatedAt: new Date().toISOString(),
+  permissions: [],
+  login: 'login',
+  password: 'password',
+  isRoot: false,
+  isActive: false,
+}
+
+const mockRecipient = {
+  _id: 'recepientId',
+  role: UserRole.RECIPIENT,
+  name: 'Recepient',
+  phone: 'phone',
+  avatar: 'avatar',
+  address: 'address',
+  vkId: 'vkId',
+  score: 0,
+  status: UserStatus.CONFIRMED,
+  location: undefined,
+  keys: false,
+  tasksCompleted: 0,
+  createdAt: new Date().toISOString(),
+  updatedAt: new Date().toISOString(),
+  permissions: [],
+  login: 'login',
+  password: 'password',
+  isRoot: false,
+  isActive: false,
+}
+
+const mockAdmin: AdminInterface = {
+  _id: 'recepientId',
+  role: UserRole.ADMIN,
+  name: 'Recepient',
+  phone: 'phone',
+  avatar: 'avatar',
+  address: 'address',
+  vkId: 'vkId',
+  createdAt: new Date().toISOString(),
+  updatedAt: new Date().toISOString(),
+  permissions: [],
+  login: 'login',
+  password: 'password',
+  isRoot: false,
+  isActive: false,
+}
 
 const mockMessages: MessageInterface[] = [
   {
-    _id: mockObjectId as any,
+    _id: new mongoose.Types.ObjectId,
     title: 'TestTitle1',
     body: 'TestBody1',
     attaches: ['Hi there!', 'Hi there!'],
     createdAt: new Date(),
-    author: {
-      _id: 'volunteerId',
-      role: UserRole.VOLUNTEER,
-      name: 'Volunteer',
-      phone: 'phone',
-      avatar: 'avatar',
-      address: 'address',
-      vkId: 'vkId',
-      score: 0,
-      status: UserStatus.BLOCKED,
-      location: undefined,
-      keys: false,
-      tasksCompleted: 0,
-      createdAt: new Date().toISOString(),
-      updatedAt: new Date().toISOString(),
-      permissions: [],
-      login: 'login',
-      password: 'password',
-      isRoot: false,
-      isActive: false,
-    },
-    chatId: mockObjectId as any,
+    author: mockVolunteer,
+    chatId: new mongoose.Types.ObjectId,
   },
   {
-    _id: mockObjectId as any,
+    _id: new mongoose.Types.ObjectId,
     title: 'TestTitle2',
     body: 'TestBody2',
     attaches: ['Hi there!', 'Hi there!'],
     createdAt: new Date(),
-    author: {
-      _id: 'volunteerId',
-      role: UserRole.VOLUNTEER,
-      name: 'Volunteer',
-      phone: 'phone',
-      avatar: 'avatar',
-      address: 'address',
-      vkId: 'vkId',
-      score: 0,
-      status: UserStatus.BLOCKED,
-      location: undefined,
-      keys: false,
-      tasksCompleted: 0,
-      createdAt: new Date().toISOString(),
-      updatedAt: new Date().toISOString(),
-      permissions: [],
-      login: 'login',
-      password: 'password',
-      isRoot: false,
-      isActive: false,
-    },
-    chatId: mockObjectId as any,
+    author: mockRecipient,
+    chatId: new mongoose.Types.ObjectId,
   },
 ]
 
@@ -79,6 +102,67 @@ const mockResponseMessage: wsMessageData = {
   data: {
     messages: mockMessages
   }
+}
+
+const mockTaskChatMeta: TaskChatMetaInterface = {
+  _id: new mongoose.Types.ObjectId().toHexString(),
+  type: 'TASK_CHAT',
+  isActive: true,
+  taskId: new mongoose.Types.ObjectId,
+  volunteer: mockVolunteer,
+  recipient: mockRecipient,
+  id: 'test',
+  createdAt: new Date().toISOString(),
+  updatedAt: new Date().toISOString(),
+  watermark: '',
+  unreads: 1,
+}
+
+const mockSystemChatMeta: SystemChatMetaInterface = {
+  _id: new mongoose.Types.ObjectId().toHexString(),
+  type: 'SYSTEM_CHAT',
+  isActive: true,
+  user: mockRecipient,
+  admin: mockAdmin,
+  id: 'test2',
+  createdAt: new Date().toISOString(),
+  updatedAt: new Date().toISOString(),
+  watermark: '',
+  unreads: 1,
+}
+
+const mockVolunteerChat: VolunteerConflictChatMetaInterface = {
+  _id: new mongoose.Types.ObjectId().toHexString(),
+  type: "CONFLICT_CHAT_WITH_VOLUNTEER",
+  isActive: true,
+  volunteer: mockVolunteer,
+  id: '10',
+  createdAt: new Date().toISOString(),
+  updatedAt: new Date().toISOString(),
+  watermark: 'volunteerWatermark',
+  unreads: 10,
+}
+
+const mockRecipientChat: RecipientConflictChatMetaInterface = {
+  _id: new mongoose.Types.ObjectId().toHexString(),
+  type: "CONFLICT_CHAT_WITH_RECIPIENT",
+  isActive: true,
+  recipient: mockRecipient,
+  id: '20',
+  createdAt: new Date().toISOString(),
+  updatedAt: new Date().toISOString(),
+  watermark: 'recipientWatermark',
+  unreads: 10,
+}
+
+const mockConflictChats: ConflictChatsTupleMetaInterface = {
+  moderator: mockAdmin,
+  taskId: 'test3',
+  adminVolunteerWatermark: 'adminVolunteerWatermark',
+  adminVolunteerUnreads: 10,
+  adminRecipientWatermark: 'adminRecipientWatermark',
+  adminRecipientUnreads: 11,
+  meta: [mockVolunteerChat, mockRecipientChat]
 }
 //#endregion
 
@@ -89,7 +173,7 @@ export class ChatService {
     private readonly queryBus: QueryBus
   ) {}
 
-  async createTaskChat(metadata: TaskChatMetaInterface): Promise<wsMessageData> {
+  async createTaskChat(metadata: TaskChatMetaInterface): Promise<TaskChatMetaInterface> {
     // const chatEntity = new ChatEntity();
 
     // const newChat = await chatEntity.createChat('TASK_CHAT', metadata);
@@ -105,17 +189,17 @@ export class ChatService {
     // return response;
 
     console.log('Creating task chat');
-    return mockResponseMessage;
+    return mockTaskChatMeta;
   }
 
-  async createSystemChat(metadata: SystemChatMetaInterface): Promise<wsMessageData> {
+  async createSystemChat(metadata: SystemChatMetaInterface): Promise<SystemChatMetaInterface> {
     console.log('Creating system chat');
-    return mockResponseMessage;
+    return mockSystemChatMeta;
   }
 
-  async createConflictChat(metadata: ConflictChatsTupleMetaInterface): Promise<wsMessageData> {
+  async createConflictChat(metadata: ConflictChatsTupleMetaInterface): Promise<ConflictChatsTupleMetaInterface> {
     console.log('Creating conflict chat');
-    return mockResponseMessage;
+    return mockConflictChats;
   }
 
   async addMessage(chatId: ObjectId, message: MessageInterface): Promise<wsMessageData> {
