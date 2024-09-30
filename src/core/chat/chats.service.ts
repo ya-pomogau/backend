05042994e8169afd6088eaebe16860eff085088e
ivanna-balanyuk <ivannabalanyuk@yandex.ru>
2024-09-30 10:40:58 +0300

    fix: linter bugs at chatsService

--- src/core/chat/chats.service.ts
+++ src/core/chat/chats.service.ts
@@ -2,18 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import mongoose, { ObjectId } from 'mongoose';
 
-import { ConflictChatsTupleMetaInterface,
+import {
+  ConflictChatsTupleMetaInterface,
   MessageInterface,
   RecipientConflictChatMetaInterface,
   SystemChatMetaInterface,
   TaskChatMetaInterface,
-  VolunteerConflictChatMetaInterface
-} from 'src/common/types/chats.types';
-import { AdminInterface, UserRole, UserStatus } from 'src/common/types/user.types';
-import { wsChatPageQueryPayload, wsMessageData } from 'src/common/types/websockets.types';
+  VolunteerConflictChatMetaInterface,
+} from '../../common/types/chats.types';
+import { AdminInterface, UserRole, UserStatus } from '../../common/types/user.types';
+import { wsChatPageQueryPayload } from '../../common/types/websockets.types';
 
-
-//#region Mock data
+// #region Mock data
 
 const mockVolunteer = {
   _id: 'volunteerId',
@@ -35,7 +35,7 @@ const mockVolunteer = {
   password: 'password',
   isRoot: false,
   isActive: false,
-}
+};
 
 const mockRecipient = {
   _id: 'recepientId',
@@ -57,7 +57,7 @@ const mockRecipient = {
   password: 'password',
   isRoot: false,
   isActive: false,
-}
+};
 
 const mockAdmin: AdminInterface = {
   _id: 'recepientId',
@@ -74,36 +74,40 @@ const mockAdmin: AdminInterface = {
   password: 'password',
   isRoot: false,
   isActive: false,
-}
-
-
+};
 
 const mockMessages = [
   {
     _id: new mongoose.Types.ObjectId().toHexString(),
     title: 'TestTitle1',
     body: 'TestBody1',
-    attaches: [new mongoose.Types.ObjectId().toHexString(), new mongoose.Types.ObjectId().toHexString()],
+    attaches: [
+      new mongoose.Types.ObjectId().toHexString(),
+      new mongoose.Types.ObjectId().toHexString(),
+    ],
     createdAt: new Date(),
     author: mockVolunteer,
     chatId: new mongoose.Types.ObjectId().toHexString(),
   },
   {
-    _id: new mongoose.Types.ObjectId,
+    _id: new mongoose.Types.ObjectId().toHexString(),
     title: 'TestTitle2',
     body: 'TestBody2',
-    attaches: [new mongoose.Types.ObjectId().toHexString(), new mongoose.Types.ObjectId().toHexString()],
+    attaches: [
+      new mongoose.Types.ObjectId().toHexString(),
+      new mongoose.Types.ObjectId().toHexString(),
+    ],
     createdAt: new Date(),
     author: mockRecipient,
     chatId: new mongoose.Types.ObjectId().toHexString(),
   },
-]
+];
 
 const mockResponseMessage = {
   data: {
-    messages: mockMessages
-  }
-}
+    messages: mockMessages,
+  },
+};
 
 const mockTaskChatMeta = {
   _id: new mongoose.Types.ObjectId().toHexString(),
@@ -117,7 +121,7 @@ const mockTaskChatMeta = {
   updatedAt: new Date().toISOString(),
   watermark: new mongoose.Types.ObjectId().toHexString(),
   unreads: 1,
-}
+};
 
 const mockSystemChatMeta: SystemChatMetaInterface = {
   _id: new mongoose.Types.ObjectId().toHexString(),
@@ -130,11 +134,11 @@ const mockSystemChatMeta: SystemChatMetaInterface = {
   updatedAt: new Date().toISOString(),
   watermark: new mongoose.Types.ObjectId().toHexString(),
   unreads: 1,
-}
+};
 
 const mockVolunteerChat: VolunteerConflictChatMetaInterface = {
   _id: new mongoose.Types.ObjectId().toHexString(),
-  type: "CONFLICT_CHAT_WITH_VOLUNTEER",
+  type: 'CONFLICT_CHAT_WITH_VOLUNTEER',
   isActive: true,
   volunteer: mockVolunteer,
   id: '10',
@@ -142,11 +146,11 @@ const mockVolunteerChat: VolunteerConflictChatMetaInterface = {
   updatedAt: new Date().toISOString(),
   watermark: new mongoose.Types.ObjectId().toHexString(),
   unreads: 10,
-}
+};
 
 const mockRecipientChat: RecipientConflictChatMetaInterface = {
   _id: new mongoose.Types.ObjectId().toHexString(),
-  type: "CONFLICT_CHAT_WITH_RECIPIENT",
+  type: 'CONFLICT_CHAT_WITH_RECIPIENT',
   isActive: true,
   recipient: mockRecipient,
   id: '20',
@@ -154,7 +158,7 @@ const mockRecipientChat: RecipientConflictChatMetaInterface = {
   updatedAt: new Date().toISOString(),
   watermark: new mongoose.Types.ObjectId().toHexString(),
   unreads: 10,
-}
+};
 
 const mockConflictChats: ConflictChatsTupleMetaInterface = {
   moderator: mockAdmin,
@@ -163,19 +167,17 @@ const mockConflictChats: ConflictChatsTupleMetaInterface = {
   adminVolunteerUnreads: 10,
   adminRecipientWatermark: 'adminRecipientWatermark',
   adminRecipientUnreads: 11,
-  meta: [mockVolunteerChat, mockRecipientChat]
-}
+  meta: [mockVolunteerChat, mockRecipientChat],
+};
 
 const mockMetaArray = [mockTaskChatMeta, mockSystemChatMeta, mockVolunteerChat];
-//#endregion
+// #endregion
 
 @Injectable()
 export class ChatService {
-  constructor(
-    private readonly commandBus: CommandBus,
-    private readonly queryBus: QueryBus
-  ) {}
+  constructor(private readonly commandBus: CommandBus, private readonly queryBus: QueryBus) {}
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async createTaskChat(metadata: TaskChatMetaInterface) {
     // const chatEntity = new ChatEntity();
 
@@ -191,20 +193,28 @@ export class ChatService {
 
     // return response;
 
+    // eslint-disable-next-line no-console
     console.log('Creating task chat');
     return mockTaskChatMeta;
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async createSystemChat(metadata: SystemChatMetaInterface): Promise<SystemChatMetaInterface> {
+    // eslint-disable-next-line no-console
     console.log('Creating system chat');
     return mockSystemChatMeta;
   }
 
-  async createConflictChat(metadata: ConflictChatsTupleMetaInterface): Promise<ConflictChatsTupleMetaInterface> {
+  async createConflictChat(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    metadata: ConflictChatsTupleMetaInterface
+  ): Promise<ConflictChatsTupleMetaInterface> {
+    // eslint-disable-next-line no-console
     console.log('Creating conflict chat');
     return mockConflictChats;
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async addMessage(chatId: string, message: MessageInterface) {
     // const chatEntity = new ChatEntity();
 
@@ -221,68 +231,93 @@ export class ChatService {
 
     // return response;
 
+    // eslint-disable-next-line no-console
     console.log('Adding message into chat');
     return mockResponseMessage;
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async getConflictChatsByAdmin(userId: ObjectId, chatQuery: wsChatPageQueryPayload) {
+    // eslint-disable-next-line no-console
     console.log('Getting conflict chats by admin');
     return mockResponseMessage;
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async getConflictClosedChats(chatQuery: wsChatPageQueryPayload) {
+    // eslint-disable-next-line no-console
     console.log('Getting conflict closed chats');
     return mockResponseMessage;
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async getOpenSystemChats(chatQuery: wsChatPageQueryPayload) {
+    // eslint-disable-next-line no-console
     console.log('Getting open system chats');
     return mockResponseMessage;
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async getClosedSystemChats(chatQuery: wsChatPageQueryPayload) {
+    // eslint-disable-next-line no-console
     console.log('Getting closed system chats');
     return mockResponseMessage;
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async getSystemChatsByUser(userId: ObjectId, chatQuery: wsChatPageQueryPayload) {
+    // eslint-disable-next-line no-console
     console.log('Getting system chats by user');
     return mockResponseMessage;
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async getOpenSystemChatsByAdmin(userId: ObjectId, chatQuery: wsChatPageQueryPayload) {
+    // eslint-disable-next-line no-console
     console.log('Getting open system chats by admin');
     return mockResponseMessage;
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async getClosedSystemChatsByAdmin(userId: ObjectId, chatQuery: wsChatPageQueryPayload) {
+    // eslint-disable-next-line no-console
     console.log('Getting closed system chats by admin');
     return mockResponseMessage;
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async closeChatByTask(taskId: ObjectId) {
     // const chatEntity = new ChatEntity();
 
     // const chat = await chatEntity.findChatByParams({ taskId: taskId });
     // chat.closeChat();
 
+    // eslint-disable-next-line no-console
     console.log('Closing chat by task');
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async closeConflictChats(chatId: string) {
+    // eslint-disable-next-line no-console
     console.log('Closing conflict chats');
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async closeSystemChat(chatId: string) {
+    // eslint-disable-next-line no-console
     console.log('Closing system chat');
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async getUserChatsMeta(userId: string | ObjectId) {
+    // eslint-disable-next-line no-console
     console.log('Getting user meta');
     return mockMetaArray;
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async getMessages(chatId: string | ObjectId, skip: number, limit?: number) {
+    // eslint-disable-next-line no-console
     console.log('Getting messages');
     return mockResponseMessage;
   }
