a3c203860b9009289d3a4d48fb540b69d816e79a
Alexey Efimov <alex-vngd@yandex.ru>
2024-10-13 19:52:20 +0300

    убирает валидацию class-validator в CreatedTaskDto, исправляет валидацию поля location

--- src/api/recipient-api/dto/created-task.dto.ts
+++ src/api/recipient-api/dto/created-task.dto.ts
@@ -1,12 +1,4 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsDefined,
-  IsNotEmpty,
-  IsString,
-  IsOptional,
-  IsDateString,
-  IsBoolean,
-} from 'class-validator';
 import { PointGeoJSONDto } from '../../../common/dto/api.dto';
 import { ResolveStatus, TaskReport, TaskStatus } from '../../../common/types/task.types';
 import { CategoryDto } from '../../../common/dto/category.dto';
@@ -14,69 +6,47 @@ import { InTaskUserDto } from './in-task-user.dto';
 
 export class CreatedTaskDto {
   @ApiProperty()
-  @IsDefined()
-  @IsNotEmpty()
-  @IsString()
   address: string;
 
   @ApiProperty()
-  @IsNotEmpty()
-  @IsString()
   status: TaskStatus.CREATED;
 
   @ApiProperty()
-  @IsNotEmpty()
   adminResolve: ResolveStatus | null;
 
   @ApiProperty()
-  @IsNotEmpty()
   moderator: InTaskUserDto | null;
 
   @ApiProperty()
-  @IsNotEmpty()
   category: CategoryDto;
 
   @ApiProperty()
-  @IsOptional()
-  @IsDateString()
   date: Date | null;
 
   @ApiProperty({
     type: PointGeoJSONDto,
   })
-  @IsNotEmpty()
   location: PointGeoJSONDto;
 
   @ApiProperty()
-  @IsNotEmpty()
   recipient: InTaskUserDto;
 
   @ApiProperty()
-  @IsNotEmpty()
   recipientReport: TaskReport | null;
 
   @ApiProperty()
-  @IsNotEmpty()
   volunteer: InTaskUserDto | null;
 
   @ApiProperty()
-  @IsNotEmpty()
   volunteerReport: TaskReport | null;
 
   @ApiProperty()
-  @IsNotEmpty()
-  @IsBoolean()
   isPendingChanges: boolean;
 
   @ApiProperty()
-  @IsDefined()
-  @IsNotEmpty()
-  @IsString()
   description: string;
 
   @ApiProperty()
-  @IsNotEmpty()
-  @IsString()
   _id: string;
 
   @ApiProperty()
--- src/common/dto/api.dto.ts
+++ src/common/dto/api.dto.ts
@@ -3,14 +3,22 @@
 import { IsIn, IsNotEmpty, IsOptional, IsPhoneNumber, IsString, IsUrl } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { NewProfileInterface } from '../types/api.types';
-import { GeoCoordinates, PointGeoJSONInterface } from '../types/point-geojson.types';
+import { PointGeoJSONInterface } from '../types/point-geojson.types';
 import { IsCoords } from '../decorators/is-coords';
 
 export class PointGeoJSONDto implements PointGeoJSONInterface {
   @IsCoords()
   @IsNotEmpty()
-  @ApiProperty()
-  coordinates: GeoCoordinates;
+  @ApiProperty({
+    type: 'array',
+    items: {
+      type: 'number',
+    },
+    minItems: 2,
+    maxItems: 2,
+    example: [58, 58],
+  })
+  coordinates: [number, number];
 
   @ApiProperty()
   @IsIn(['Point'])
